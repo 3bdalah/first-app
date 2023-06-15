11ec4b513e0e236b1569a0aa36ac5c741fac0647
3bdalah <3bdallah.dev@gmail.com>
2023-06-15 11:14:10 +0300

    update : app.js - update options by creating by array from

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,25 @@
 // import './index.css';
+const initialItems = [
+  {
+    id : 1,description : "Passports" , quantity : 2, packed :false
+  },
+  {
+    id : 2 , description : "Socks", quantity : 12 , packed : true
+  },
+  {
+    id : 3,description : "Passports" , quantity : 2, packed :false
+  },
+  {
+    id : 4 , description : "Socks", quantity : 12 , packed : true
+  },
+  {
+    id : 5,description : "Passports" , quantity : 2, packed :false
+  },
+  {
+    id : 6 , description : "Socks", quantity : 12 , packed : true
+  },
+]
+
 
 function App() {
   return (
@@ -22,6 +43,8 @@ function Form (){
   return (
     <div className="add-form">
       <h3>What do you need for your Trip?</h3>
+     <select>{Array.from( {length: 20}, (_,i) => i+1).map(num => <option value={num} key={num}>{num}</option>)}</select>
+    
     </div>
   )
 }
@@ -29,8 +52,20 @@ function Form (){
 function PackingList (){
   return(
     <div className="list">
-      list 
+      <ul>
+        {initialItems.map(item =>  <Item item={item}/>)}
+      </ul>
     </div>
+  )
+}
+function Item ({item}) {
+  return(
+    <li>
+      <span style={item.packed ? {textDecoration : "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>🎯</button>
+    </li>
   )
 }
 
