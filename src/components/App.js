05b77b80daa87da71fd26d43f044a331629e2174
3bdalah <3bdallah.dev@gmail.com>
2023-06-15 10:16:35 +0300

    update: app.js : - adding function logo - adding function form - adding function packing list - adding stats

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,43 @@
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="app">
+      <Logo />
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
   );
 }
 
+
+function Logo (){
+  return(
+    <h1> 🐱‍🏍Far Away 🛺</h1>
+  )
+}
+
+function Form (){
+  return (
+    <div className="add-form">
+      <h3>What do you need for your Trip?</h3>
+    </div>
+  )
+}
+
+function PackingList (){
+  return(
+    <div className="list">
+      list 
+    </div>
+  )
+}
+
+function Stats(){
+  return (
+    <footer className="stats">
+     <em> 👜you have X items on your list and you already packed x (x%) </em>
+    </footer>
+  )
+}
 export default App;
